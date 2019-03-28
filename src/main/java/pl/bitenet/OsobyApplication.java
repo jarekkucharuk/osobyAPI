5b2761c95fc8d08e38b99eca7538a3feea53cbb7
jarekkucharuk <jarekkucharuk@gmail.com>
2019-03-28 18:53:56 +0100

    initial commit

---
+++ src/main/java/pl/bitenet/OsobyApplication.java
@@ -0,0 +1,13 @@
+package pl.bitenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsobyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsobyApplication.class, args);
+	}
+
+}
